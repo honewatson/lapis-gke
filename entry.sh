e51259e872f077630e4ede3529b4112ab7ffa851
honewatson <hone@hone.be>
2017-09-12 00:00:47 +1000

    Add pod and service yaml

--- entry.sh
+++ entry.sh
@@ -9,11 +9,12 @@ GIT_DIR_VC_DIR=$GIT_DIR/.git
 echo $GIT_DIR_VC_DIR
 echo $GIT_REPO
 echo $GIT_DIR
-
+ls -la $GIT_DIR
 if [ ! -d $GIT_DIR_VC_DIR ]
 then
-    git clone $GIT_REPO $GIT_DIR
     cd $GIT_DIR
+    rm -rf *
+    git clone $GIT_REPO ./
     moonc .
 else
     cd $GIT_DIR
